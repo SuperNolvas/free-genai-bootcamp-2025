0846d7ca2b4608f5e408980ca8737336d6b57de4
Phil Evans <pde_repair@hotmail.com>
2025-02-16 12:40:26 +0000

    Update comps api import dependencies

--- opea-comps/megaservice/app.py
+++ opea-comps/megaservice/app.py
@@ -1,4 +1,5 @@
-from comps import MicroService, ServiceOrchestrator, ServiceType
+from comps import MicroService, ServiceOrchestrator, ServiceType, ServiceRoleType
+from comps.cores.proto.api_protocol import ChatCompletionRequest, ChatCompletionResponse
 
 import os
 
@@ -33,3 +34,23 @@ class ExampleService:
         )
         self.megaservice.add(embedding).add(llm)
         self.megaservice.flow_to(embedding, llm)
+
+    def start(self):
+
+        self.service = MicroService(
+            self.__class__.__name__,
+            service_role=ServiceRoleType.MEGASERVICE,
+            host=self.host,
+            port=self.port,
+            endpoint=self.endpoint,
+            input_datatype=ChatCompletionRequest,
+            output_datatype=ChatCompletionResponse,
+        )
+
+        self.service.add_route(self.endpoint, self.handle_request, methods=["POST"])
+
+        self.service.start()
+
+        example = ExampleService()
+        example.add_remote_service()
+        example.megaservice.run()
